b2227492d87aa2b0b3f569be373760b35dda87a8
Zhongrui Li <GODLZR@Zhongrui-Lis-MacBook-Pro.local>
2015-03-24 16:22:21 -0400

    The update of Q1.1

--- src/com/ArrayString/DataStructures/stringHelper.java
+++ src/com/ArrayString/DataStructures/stringHelper.java
@@ -1,9 +1,22 @@
+/*
+ * The implementation of the Questions in "Cracking the Coding Interview"
+ * Data Structures
+ * Chapter 1
+ * 1.1 Implement an algorithm to determine if a string has all unique characters. 
+ * What if you cannot use additional data structures?
+ * boolean uniqueCharacter(String s);
+ * 
+ */
+
 package com.ArrayString.DataStructures;
 
 public class stringHelper {
 	
 	public boolean uniqueCharacter(String s) {
-		
+/*
+ * 1.1 Implement an algorithm to determine if a string has all unique characters. 
+ * What if you cannot use additional data structures?
+ */
 		boolean a[];
 		
 		a = new boolean[256];
